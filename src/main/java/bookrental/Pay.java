fcdf71ef0a20ad1ab55fff4ff5152d9e1ad1b687
04950 <yeonkyung.park@sk.com>
2020-10-21 16:46:11 +0900

    1st commit

---
+++ src/main/java/bookrental/Pay.java
@@ -0,0 +1,85 @@
+package bookrental;
+
+import javax.persistence.*;
+import org.springframework.beans.BeanUtils;
+import java.util.List;
+
+@Entity
+@Table(name="Pay_table")
+public class Pay {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String status;
+    private Long bookId;
+    private String payDate;
+    private String payCancelDate;
+    private Double bookPrice;
+
+    @PostPersist
+    public void onPostPersist(){
+        Paid paid = new Paid();
+        BeanUtils.copyProperties(this, paid);
+        paid.publishAfterCommit();
+
+
+    }
+
+    @PostUpdate
+    public void onPostUpdate(){
+        PayCanceled payCanceled = new PayCanceled();
+        BeanUtils.copyProperties(this, payCanceled);
+        payCanceled.publishAfterCommit();
+
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+    public String getPayDate() {
+        return payDate;
+    }
+
+    public void setPayDate(String payDate) {
+        this.payDate = payDate;
+    }
+    public String getPayCancelDate() {
+        return payCancelDate;
+    }
+
+    public void setPayCancelDate(String payCancelDate) {
+        this.payCancelDate = payCancelDate;
+    }
+    public Double getBookPrice() {
+        return bookPrice;
+    }
+
+    public void setBookPrice(Double bookPrice) {
+        this.bookPrice = bookPrice;
+    }
+
+
+
+
+}
